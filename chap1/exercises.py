6d5387b540970bcf841f820baf0d8fc245c18c70
Frode Thorsen Børseth <frodetb@gmail.com>
2020-05-10 18:39:15 +0200

    Improve output from exercises.py

--- chap1/exercises.py
+++ chap1/exercises.py
@@ -1,10 +1,15 @@
+from os import popen
 from string import ascii_lowercase as SYMBOLS
 from string import ascii_uppercase as UPPER_SYMBOLS
-from typing import Optional
+from typing import Any, Callable, Optional
 
 
 CRYPT_COLS = 10
 M = len(SYMBOLS)
+
+# A small help-function for pretty printing:
+N_COLS = int(popen("stty size", "r").read().split()[1])
+print_center = lambda s, f: print(str.center(s, N_COLS, f))
 
 
 class CipherWheel:
@@ -34,65 +39,74 @@ def print_crypt(text: str) -> None:
         print(char, end="")
         if i % 5 == 4:
             print(" ", end="")
-        if i % (5*CRYPT_COLS) == 5*CRYPT_COLS - 1:
+        if i % (5 * CRYPT_COLS) == 5 * CRYPT_COLS - 1:
             print("\n", end="")
     print()
-    
 
-def exercise_1_1():
-    # a)
+
+def excercise_header(text: str) -> Callable:
+    def decorator(f: Callable) -> Callable:
+        def wrapper(skip: Optional[bool] = False, **kwargs: Any) -> None:
+            print_center(text, "-")
+            if skip:
+                print("(skipping)\n")
+                return
+            f(**kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@excercise_header(" Exercise 1.1. ")
+def exercise_1_1() -> None:
+    def print_helper(encr: str, decr: str, message: Optional[str] = None) -> None:
+        if message:
+            print(message)
+        print_crypt(encr)
+        print_crypt(decr)
+        print()
+
+    print("a)")
     cipher_wheel = CipherWheel(11)
     message = "A page of history is worth a volume of logic."
     encrypted = cipher_wheel.encrypt(message)
     decrypted = cipher_wheel.decrypt(encrypted)
-    print(message)
-    print_crypt(encrypted)
-    print_crypt(decrypted)
-    print()
+    print_helper(encrypted, decrypted, message)
 
-    # b)
+    print("b)")
     cipher_wheel.set_shift(7)
     encrypted = "AOLYLHYLUVZLJYLAZILAALYAOHUAOLZLJYLAZAOHALCLYFIVKFNBLZZLZ"
     decrypted = cipher_wheel.decrypt(encrypted)
-    # -> therearenosecretsbetterthanthesecretsthateverybodyguesses
-    print_crypt(encrypted)
-    print_crypt(decrypted)  
-    print()
+    print_helper(encrypted, decrypted)
 
-    # c)
+    print("c)")
     encrypted = "XJHRFTNZHMZGAHIUETXZJNBWNUTRHEPOMDNBJMAUGORFAOIZOCC"
     decrypted = ""
     for i, char in enumerate(encrypted):
         cipher_wheel.set_shift(i + 1)
         decrypted += cipher_wheel.decrypt(char)
-    # -> whenangrycounttenbeforeyouspeakifveryangryanhundred
-    print_crypt(encrypted)
-    print_crypt(decrypted)  
-    print()
+    print_helper(encrypted, decrypted)
 
 
-def exercise_1_2():
+@excercise_header(" Exercise 1.2. ")
+def exercise_1_2(show_only_solution: bool = True) -> None:
     cipher_wheel = CipherWheel(0)
-    show_solution = True
-    inputs = [
-        ("LWKLQNWKDWLVKDOOQHYHUVHHDELOOERDUGORYHOBDVDWUHH", 3),
-        # ithinkthatishallneverseeabillboardlovelyasatree
-        ("UXENRBWXCUXENFQRLQJUCNABFQNWRCJUCNAJCRXWORWMB", 9),
-        # loveisnotlovewhichalterswhenitalterationfinds
-        ("BGUTBMBGZTFHNLXMKTIPBMAVAXXLXTEPTRLEXTOXKHHFYHKMAXFHNLX", 19),
-        # inbaitingamousetrapwithcheesealwaysleaveroomforthemouse
-    ]   
 
-    def helper(text: str, solution: Optional[int]) -> None:
-        for i in ([solution] if solution else range(1, M)):
+    inputs = [
+        ("a)", "LWKLQNWKDWLVKDOOQHYHUVHHDELOOERDUGORYHOBDVDWUHH", 3),
+        ("b)", "UXENRBWXCUXENFQRLQJUCNABFQNWRCJUCNAJCRXWORWMB", 9),
+        ("c)", "BGUTBMBGZTFHNLXMKTIPBMAVAXXLXTEPTRLEXTOXKHHFYHKMAXFHNLX", 19),
+    ]
+    for problem_name, encrypted, solution in inputs:
+        print(problem_name)
+        for i in [solution] if show_only_solution else range(1, M):
             cipher_wheel.set_shift(i)
             print_crypt(cipher_wheel.decrypt(encrypted))
-
-    for encrypted, solution in inputs:
-        helper(encrypted, solution if show_solution else None)
         print()
 
 
 if __name__ == "__main__":
-    # exercise_1_1()
-    exercise_1_2()
+    print_center(" Exercises chapter 1 ", "=")
+    exercise_1_1()  # skip=True)
+    exercise_1_2(show_only_solution=False)  # skip=True)
